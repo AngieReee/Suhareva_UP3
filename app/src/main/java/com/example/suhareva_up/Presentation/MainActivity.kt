a134d318f102d9593f9244615ff68eb4861bf312
AngelikaSuhareva <asuhareva356@gmail.com>
2025-03-17 22:27:23 +0300

    modified project structure

--- app/src/main/java/com/example/suhareva_up/Presentation/MainActivity.kt
+++ app/src/main/java/com/example/suhareva_up/Presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.suhareva_up
+package com.example.suhareva_up.Presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.suhareva_up.ui.theme.Suhareva_UPTheme
+import com.example.suhareva_up.Presentation.ui.theme.Suhareva_UPTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/suhareva_up/Presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/suhareva_up/Presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.suhareva_up.ui.theme
+package com.example.suhareva_up.Presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
